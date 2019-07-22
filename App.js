819c89d44749cc489139bbaa9a3fbcb1c6219186
Yoga Setiawan <mryoga1995@gmail.com>
2019-07-23 00:53:08 +0700

    add OneSignal

--- App.js
+++ App.js
@@ -1,7 +1,36 @@
 import React, {Component} from 'react';
 import AppNavigator from './src/routes/rootNavigator';
+import OneSignal from 'react-native-onesignal';
 
 export default class App extends Component {
+  constructor(properties) {
+    super(properties);
+    OneSignal.init("85aa6769-11fa-48f8-ac83-bbe3cc4a1889");
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
   render() {
     return (
       <AppNavigator />
--- android/app/src/main/java/com/ekarcis/MainApplication.java
+++ android/app/src/main/java/com/ekarcis/MainApplication.java
@@ -3,6 +3,7 @@ package com.ekarcis;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new RNGestureHandlerPackage()
       );
     }

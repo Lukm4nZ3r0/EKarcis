81d9bfc20b8f7fe12dde4d624c03bfc87846f687
lukm4nz3r0 <laruku.asep@gmail.com>
2019-07-25 12:07:01 +0700

    new commit

--- src/public/redux/actions/URL.js
+++ src/public/redux/actions/URL.js
@@ -1,8 +1,8 @@
 // AWS
-// const URL = 'http://52.27.82.154:7000'
+const URL = 'http://52.27.82.154:7000'
 
 // Yoga
-const URL = 'http://192.168.6.101:7000'
+// const URL = 'http://192.168.6.101:7000'
 
 // Samid
 // const URL = 'http://192.168.6.115:7000'
